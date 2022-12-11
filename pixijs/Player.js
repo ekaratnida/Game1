bbdb2528a4da6a2c5b8d4451db39cab176c23f21
Ekarat <ekarat@as.nida.ac.th>
2022-12-11 12:09:37 +0700

    Design Player class

---
+++ pixijs/Player.js
@@ -0,0 +1,21 @@
+class Player extends PIXI.Sprite {
+    constructor(px,py,texture,name,hp,speed,sx,sy){
+      super(texture);
+      this.anchor.set(0.5);
+      this.name = name;
+      this.x = px;
+      this.y = py;
+      this.hp = hp;
+      this.speed = speed;
+      this.scale.x = sx;
+      this.scale.y = sy;
+    }
+
+    status() {
+      return this.name
+    }
+
+    moveLeft() {
+      this.x = this.x - this.speed;
+    }
+  }
